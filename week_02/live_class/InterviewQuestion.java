60770504b6fbc8ce6197de65a1e4c240e61beb6f
ibrahim <iboycl35@gmail.com>
2023-08-16 17:11:24 +0300

    The second week - Tuesday

---
+++ week_02/live_class/InterviewQuestion.java
@@ -0,0 +1,18 @@
+public class InterviewQuestion {
+    public static void main(String[] args){
+        int x = 5;
+        int y = 7;
+
+        x = x + y;
+
+        y = x - y;
+
+        x = x - y;
+
+        System.out.println( "y is " + y + " x is " + x);
+
+    }
+
+
+
+}
